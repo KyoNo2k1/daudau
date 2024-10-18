9934442e209621306e5757218ca1652188e9e5e5
Lê Trung Nghĩa <nghia567123@gmail.com>
2024-10-18 21:01:12 +0900

    second

--- app/page.js
+++ app/page.js
@@ -1,12 +1,27 @@
 "use client";
 import MediaGallery from "../components/MediaGallery";
 import media from "../data/media";
-
+import Starfield from "react-starfield";
 export default function Home() {
   return (
     <div>
-      <h1>Đậu Đậu ngôk 🌷</h1>
-      <MediaGallery media={media} />
+      <main className="main">
+        <div className="mainDiv">
+          <h1 className="bigTitle">
+            <span className="welcomeTexjt">Chào mừng tới </span> thế giới của
+            Đậu Đậu{" "}
+          </h1>
+          <div className="line" />
+          <h1 style={{ color: "white" }}>Đậu Đậu ngôk 🌷</h1>
+          <MediaGallery media={media} />
+        </div>
+      </main>
+      <Starfield
+        starCount={1000}
+        starColor={[255, 255, 255]}
+        speedFactor={0.05}
+        backgroundColor="black"
+      />
     </div>
   );
 }
